244ace8125aaeb2de5ecf9fb7fcedcade027f337
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-04-23 10:51:47 -0300

    Fim da aula 02: requisições em APIs, server-json, design da home criado

--- podcastr_next/src/components/Player/index.tsx
+++ podcastr_next/src/components/Player/index.tsx
@@ -2,9 +2,9 @@ import styles from './styles.module.scss'
 
 export function Player() {
 
-	return(
+	return (
 		<div className={styles.playerContainer}>
-      <header>
+			<header>
 				<img src='/playing.svg' alt='Tocando agora'></img>
 				<strong>Tocando agora</strong>
 			</header>
@@ -12,7 +12,37 @@ export function Player() {
 			<div className={styles.emptyPlayer}>
 				<strong>Selecione um podcast para ouvir</strong>
 			</div>
-			<footer></footer>
-    </div>
+			<footer className={styles.empty}>
+				<div className={styles.progress}>
+					<span>00:00</span>
+					<div className={styles.slider}>
+						<div className={styles.emptySlider}></div>
+					</div>
+					<span>00:00</span>
+				</div>
+
+				<div className={styles.buttons}>
+					<button type='button'>
+						<img src='/shuffle.svg' alt='Embaralhar'></img>
+					</button>
+
+					<button type='button'>
+						<img src='/play-previous.svg' alt='Tocar anterior'></img>
+					</button>
+
+					<button type='button' className={styles.playButton}>
+						<img src='/play.svg' alt='Tocar'></img>
+					</button>
+
+					<button type='button'>
+						<img src='/play-next.svg' alt='Tocar próximo'></img>
+					</button>
+
+					<button type='button'>
+						<img src='/repeat.svg' alt='Repetir'></img>
+					</button>
+				</div>
+			</footer>
+		</div>
 	);
 }
--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
-  return (
-    <h1>Index</h1>
-  )
+import { useEffect } from 'react'
+
+export default function Home(props) {
+
+	/* SPA
+	useEffect(() => {
+		fetch('http://localhost:3333/episodes')
+			.then(response => response.json())
+			.then(data => console.log(data))
+	})
+	*/
+	return (
+		<h1>Index</h1>
+	)
 }
+
+// SSG
+
+export async function getStaticProps() {
+	const response = await fetch('http://localhost:3333/episodes')
+	const data = await response.json()
+
+	return {
+		props: {
+			episodes: data,
+		},
+		revalidate: 60 * 60 * 8
+	}
+}
+
+
+/* SSR
+executa toda vez que alguém acessa o site
+
+export async function getSeverSideProps(){
+const response = await fetch('http://localhost:3333/episodes')
+const data = await response.json()
+
+return {
+	props:{
+		episodes: data,
+
+	}
+}
+}
+*/
